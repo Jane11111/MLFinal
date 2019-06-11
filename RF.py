3cbdc799e006f77c9b8cb4975e158234653e3193
jane11111 <791057615@qq.com>
2019-06-11 14:19:09 +0800

    random forest

--- RF.py
+++ RF.py
@@ -2,3 +2,28 @@
 # @Time    : 2019-06-11 10:08
 # @Author  : zxl
 # @FileName: RF.py
+
+from Bagging import Bagging
+from RandomTree import RandomTree
+from Util import *
+"""
+实现随机森林方法
+"""
+class RandomForest(Bagging):
+
+    def __init__(self,baselearner_num):
+        super.__init__(baselearner_num)
+        pass
+
+
+    def fit(self,dataset,label):
+        self.dataset = dataset
+        self.label = label
+
+        percent = 0.8
+        for i in range(self.num):
+            (sample_dataset, sample_label) = Bootstrap(self.dataset, self.label, percent)
+            model = RandomTree()
+            model.fit(sample_dataset, sample_label)
+            self.models.append(model)
+
--- main.py
+++ main.py
@@ -6,6 +6,7 @@
 import numpy as np
 import pandas as pd
 from DT import RT
+from RF import RandomForest
 from sklearn import tree
 from sklearn.ensemble import BaggingRegressor
 
@@ -17,7 +18,7 @@ if __name__=="__main__":
     root = "~/Documents/数据集/sale/"
     train_path = root + "train.csv"
     test_path = root + "test.csv"
-    out_file=root+"result/result-sklearn-bagging.csv"
+    out_file=root+"result/result-random-forest.csv"
 
 
     train_dataset,train_label,test_dataset=LoadData(train_path,test_path)
@@ -32,8 +33,12 @@ if __name__=="__main__":
     # model=Bagging(20)
     # model.fit(train_dataset,train_label)
     # predict_y=model.predict(test_dataset)
-    model=BaggingRegressor(n_estimators=20)
-    predict_y=model.fit(train_dataset,train_label).predict(test_dataset)
+    # model=BaggingRegressor(n_estimators=20)
+    # predict_y=model.fit(train_dataset,train_label).predict(test_dataset)
+
+    model = RandomForest(20)
+    model.fit(train_dataset,train_label)
+    predict_y = model.predict(test_dataset)
 
 
 
