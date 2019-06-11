63d404ee4edba7fe1978f0bc94ccbe2123c1afc8
jane11111 <791057615@qq.com>
2019-06-11 11:41:35 +0800

    bagging

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ if __name__=="__main__":
     root = "~/Documents/数据集/sale/"
     train_path = root + "train.csv"
     test_path = root + "test.csv"
-    out_file=root+"result/result-bagging.csv"
+    out_file=root+"result/result-sklearn-bagging.csv"
 
 
     train_dataset,train_label,test_dataset=LoadData(train_path,test_path)
@@ -29,9 +29,11 @@ if __name__=="__main__":
     # model=tree.DecisionTreeRegressor(random_state=1)
     # predict_y=model.fit(train_dataset,train_label).predict(test_dataset)
 
-    model=Bagging(20)
-    model.fit(train_dataset,train_label)
-    predict_y=model.predict(test_dataset)
+    # model=Bagging(20)
+    # model.fit(train_dataset,train_label)
+    # predict_y=model.predict(test_dataset)
+    model=BaggingRegressor(n_estimators=20)
+    predict_y=model.fit(train_dataset,train_label).predict(test_dataset)
 
 
 
